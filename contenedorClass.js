f4145808ff400f30c89a01e042aebd158105bdd5
Joaquín Carré <joa_carre21@hotmail.com>
2022-10-22 00:22:53 -0300

    Revert "Avanzando con el desafio"
    This reverts commit d7155023524ebac8ab034d816237e71c73edec20.

--- contenedorClass.js
+++ contenedorClass.js
@@ -1,16 +1,15 @@
-const fs = require('fs');
+const fs=require('fs');
 
-class Contenedor {
-    constructor(fileJSON) {
+class Contenedor{
+    constructor(fileJSON){
         this.fileJSON = fileJSON;
     }
-
     async getAll() {
         try {
-            const products = await fs.promises.readFile(this.fileJSON, 'utf-8')
-            return await JSON.parse(products)
+            const products = await fs.promises.readFile (this.fileJSON, 'utf-8')
+            return await JSON.parse (products)
         } catch (error) {
-            console.log('No es posible obtener los productos de la base de datos', error);
+            console.log('No es posible obtener los productos de la base de datos',error);
         }
     }
 
--- routers/productRoutes.js
+++ routers/productRoutes.js
@@ -5,58 +5,19 @@ const products = new Contenedor('./dataBase/products.json');
 
 const router = Router()
 
-const Admin = true
-
 router.get('/', async (req, res) => {
     const productsJSON = await products.getAll();
-    const productsTemplate = productsJSON
     try {
         const data = {
-            productsTemplate,
+            productsJSON,
             isEmpty: !productsJSON.length,
-            message: "No hay productos seleccionados",
-            isAdmin: Admin
+            isAdmin: true
         }
         res.render('products', data)
     } catch (error) {
         next(error)
     }
+    
 })
-
-router.get('/:id', async (req, res) => {
-    const { id } = req.params
-    const oneProduct = await products.getByID(id);
-    try {
-        if (!oneProduct.length) {
-            const data = {
-                isEmpty: true,
-                message: "El producto no existe",
-            }
-            res.status(404).render('products', data)
-        } else {
-            const productsTemplate = oneProduct
-            const data = {
-                productsTemplate,
-                isEmpty: false,
-                isAdmin: Admin
-            }
-            res.status(200).render('products', data)
-        }
-    } catch (error) {
-        next(error)
-    }
-})
-
-router.post('/', async (req, res) => {
-    const product = req.body
-    try {
-        const data = await products.addProduct(product)
-        res.status(data.status).render('products', data)
-    } catch (error) {
-        next(error)
-    }
-})
-
-
 
 module.exports = router
